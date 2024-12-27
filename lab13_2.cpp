80f40112079c3ad519a6c27b1e3bcb089ad252db
PatcharapaChainet <patcharapa_chainet@cmu.ac.th>
2024-12-27 15:13:00 +0700

    lab13 bef. new year

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,49 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool x[][M]){
+    cout << "------------------------------------------------------------------------"<<endl;
+    for(int i=0; i<30; i++){
+        cout<<"|";
+        for(int j=0; j<70; j++){
+            if(x[i][j]==1){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout<<"|"<<endl;
+    }
+    cout << "------------------------------------------------------------------------"<<endl;
+}
+
+void updateImage(bool image[][M],int s,int x,int y){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            double sum = pow(i-x,2)+pow(j-y,2);
+            if(sqrt(sum) <= s-1)image[i][j] = 1;
+        }
+    }
+}#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int N = 30;
+const int M = 70;
+
+void updateImage(bool [][M],int,int,int);
+
+void showImage(const bool [][M]);
+
+int main()
+{
+    bool image[N][M] = {};
+    int s,x,y;
+    do{
+        showImage(image);
+        cout << "Input your brush size and location: ";
+        cin >> s >> x >> y;
+        updateImage(image,s,x,y);
+    }while(s != 0 || x != 0 || y != 0);
+    return 0;
+}
